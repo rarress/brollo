4c366a11a6efbe9520ea394791efe8a29031f907
cosmin220304 <cosmin.aftanase@yahoo.com>
2020-07-12 00:35:27 +0300

    register

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -17,12 +17,26 @@ const controller = {
     random: (req, res) => { 
         res.json({data: "random data"})
     },
-    addUser: (req, res) => {
-        const new_user = req.body 
-        users.create(
-            new_user,
-            (err, data) => sendResponse(res, err, data)
-        )
+    register: (req, res) => {
+        try {
+            const requiredKeys = ['FirstName', 'LastName', 'Username', 'Email', 'Password']
+            let new_user = {}
+            console.log(req.body)
+            requiredKeys.forEach(key => {
+                if (req.body[key] === undefined)
+                    throw key
+                new_user[key] = req.body[key]
+            })
+            console.log(new_user)
+            users.create(
+                new_user,
+                (err, data) => sendResponse(res, err, data)
+            )
+        }
+        catch(err){
+            console.log(err)
+            res.sendStatus(400)
+        }
     }
 }
 module.exports = controller;
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({ 
-    Nume: {type: String, required: true},
-    Prenume: {type: String, required: true},
-    Username: {type: String, required: true},
-    Email: {type: String, required: true},
-    Parola: {type: String, required: true},
+    FirstName: String,
+    LastName: String,
+    Username: String,
+    Email: String,
+    Password: String,
     Verified: Boolean,
 });
 
