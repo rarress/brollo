441d06bdf9fa61ba31b511e7c5cefdadca8b26e5
Cosmin <cosmin.aftanase@yahoo.com>
2020-07-17 17:43:37 +0300

    add password encrpyt

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,71 +1,77 @@
 const users = require('../models/userModel')
 const bcrypt = require('bcrypt')
-const saltRounds = 12
+const nodemailer = require('nodemailer')
+const saltRounds = 10
 
-function sendResponse(res, err, data) {
+const sendResponse = (res, err, data) => {
     if (err)
         res.json({ success: false, message: err })
     else if (!data || data.length == 0)
         res.json({ success: false, message: "Not Found" })
     else 
-        res.json({success: true, data: data})
+        res.json({ success: true, data: data })
 }  
 
-const isObjectUnique = (object) => new Promise( 
+const isFieldUnique = (object) => new Promise( 
     (resolve, reject) => users.findOne(object).exec((err, data) => data? resolve(true) : resolve(false))
-)
+) 
 
-const encrpytPass = pass => new Promise ((resolve, reject) =>
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-        if (err)
-            reject (err)
-        bcrypt.hash(pass, salt, (err, hash) => {
-            if (err) 
-                reject (err)
-            else
-                resolve (hash)
-        })
-    })
-)
-
-
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'brolloApp@gmail.com',
+      pass: '}FyKz4uB-p)HV8^%DzpJu"sMzwNqyz[RCJq]p}+~4THDc>8X64'
+    }
+})
+  
+const mailOptions = (email) => { 
+    return {
+        from: 'brolloApp@gmail.com',
+        to: email,
+        subject: 'Brollo Account Confirmation',
+        text: 'TODO: ADD LINK!'
+    }
+}
+  
 const controller = {
-    random: (req, res) => { 
-        res.json({data: "random data"})
-    },
     register: async (req, res) => {
         try {
             const requiredKeys = ['First Name', 'Last Name', 'Username', 'Email', 'Password']
 
             // Check if all data was provided and only they the required keys
-            let new_user = {} 
-            for (key of requiredKeys) {
-                if (req.body[key] === undefined)
+            let new_user = {}  
+            for ( key of requiredKeys ) 
+            {
+                if ( req.body[key] === undefined )
                     throw `${key} key is missing!`
 
-                new_user[key] = req.body[key]
-
-                if (key === 'Password'){
-            //        new_user[key] = await encrpytPass(req.body.key)
+                if ( key === 'Password' ) { 
+                    const salt = bcrypt.genSaltSync(saltRounds)
+                    const test = bcrypt.hashSync(req.body[key], salt)
+                    console.log(test)
+                    new_user[key] = test
+                    console.log("aaa")
                 }
+                else
+                    new_user[key] = req.body[key] 
             }
             
             // Check if unique Username
-            const usernameExists = await checkIfExists( {'Username': req.body['Username']} ) 
+            const usernameExists = await isFieldUnique( {'Username': req.body['Username']} ) 
             if (usernameExists)
                 throw "Username already taken!"
 
             // Check if unique Email
-            const emailExists = await checkIfExists( {'Email': req.body['Email']} )
+            const emailExists = await isFieldUnique( {'Email': req.body['Email']} )
             if (emailExists)
                 throw "Email already taken!" 
-
-            console.log(new_user)
-            // Add user to db
-            users.create(
-                new_user,
-                (err, data) => sendResponse(res, err, data)
-            )
+ 
+            // Add user to db 
+            users.create( new_user, (err, data) => {
+                if (!err || err === undefined) 
+                    transporter.sendMail(mailOptions(new_user['Email']))
+                sendResponse(res, err, data)
+            }) 
         }
         catch(error){
             sendResponse(res, error)  
