db3df0d3567b1a0471e77f5ca3d46339d2fe42fa
Cosmin <cosmin.aftanase@yahoo.com>
2020-07-20 18:11:28 +0300

    register change

--- client/src/components/Register/Submit.js
+++ client/src/components/Register/Submit.js
@@ -3,7 +3,7 @@ import { Button, Icon } from 'react-materialize'
 import { useHistory } from "react-router-dom"
 import axios from 'axios'
 
-const Submit = ({data, setError}) => {
+const Submit = ({data, setMessage}) => {
     const history = useHistory()
 
     const allFieldCompleted = () => {
@@ -45,27 +45,28 @@ const Submit = ({data, setError}) => {
 
     const onSubmit = () => {
         if (!allFieldCompleted()) {
-            setError("All fields are required!")
+            setMessage("All fields are required!")
             return
         }
         
         if (!isValidEmail()) {
-            setError("Invalid email!")
+            setMessage("Invalid email!")
             return
         }
         
         if (!isValidPassword()) {
-            setError("Password is too weak![ENDLINE]Make sure to have at least 8 characters, 1 lowercase letter, 1 uppercase letter and 1 digit")
+            setMessage("Password is too weak![ENDLINE]Make sure to have at least 8 characters, 1 lowercase letter, 1 uppercase letter and 1 digit")
             return
         }
 
          if (!passwordsAreEqual()) {
-            setError("The passwords are not equal!")
+            setMessage("The passwords are not equal!")
             return
         }
-           
+        
+        setMessage("loading")
         axios.post( '/api/register', JSON.stringify(data), { headers: {"Content-Type" : "application/json"} } )
-             .then( ({data}) => data.success === true? history.push("/login") : setError(data.message) )
+             .then( ({data}) => data.success === true? history.push("/login") : setMessage(data.message) )
              .catch( (err) => history.push("/error") )
     }
 
--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react'  
 import { Col, Row, CardPanel} from 'react-materialize'
-import ErrorMessage from './ErrorMessage'
+import Message from './Message'
 import Field from './Field'
 import Submit from './Submit'
 
 const Register = () => {
   const [data, setData] = useState({}) 
-  const [error, setError] = useState("")
+  const [message, setMessage] = useState("")
 
   useEffect(() => {
     document.body.style.backgroundImage = "url(/purpleForest.jpg)"
@@ -23,14 +23,14 @@ const Register = () => {
     <Col className="col s6 center-align">
       <h1 className="specialTextColor"> Register </h1>
       <CardPanel className="green lighten-5 roundBorder">  
-        <ErrorMessage error={error}/>
+        <Message message={message}/>
         <Field placeholder="First Name" txtInputHndler={txtInputHndler}/> 
         <Field placeholder="Last Name" txtInputHndler={txtInputHndler}/> 
         <Field placeholder="Email" email={true} txtInputHndler={txtInputHndler}/>    
         <Field placeholder="Username" txtInputHndler={txtInputHndler}/>     
         <Field placeholder="Password" password={true} txtInputHndler={txtInputHndler}/>     
         <Field placeholder="Confirm Password" password={true} txtInputHndler={txtInputHndler}/> 
-        <Submit data={data} setError={setError}/>  
+        <Submit data={data} setMessage={setMessage}/>  
       </CardPanel>  
     </Col>
   </Row> 
