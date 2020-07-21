7613b4653157939910e1df1e86ae9ea2ea6dcdd3
Cosmin <cosmin.aftanase@yahoo.com>
2020-07-21 23:32:55 +0300

    email verification message

--- client/src/components/HomeNotLogged/index.js
+++ client/src/components/HomeNotLogged/index.js
@@ -1,19 +1,22 @@
 import React, {useState, useEffect} from 'react' 
 import {Row} from 'react-materialize'
 import Showcase from './Showcase'
+import Cookies from 'js-cookie';
 
-const Home = ({isLoggedIn}) => { 
-  const [message, setMessage] = useState()
-  isLoggedIn = true
+const Home = () => { 
+  const [message, setMessage] = useState() 
 
   useEffect(() => {
     document.body.style.backgroundImage = "url(purpleForest.jpg)"
+    const newMessage = Cookies.get('HomeNotLogged_message')
+    Cookies.remove('HomeNotLogged_message')
+    console.log(newMessage)
   }, [])
 
   return (
   <>
     { message? <Row>{message}</Row> : null }
-    { isLoggedIn? <Showcase/> : null }
+    <Showcase/>
   </>
   ); 
 }
--- server/controllers/verifyUserController.js
+++ server/controllers/verifyUserController.js
@@ -1,12 +1,15 @@
 const users = require('../models/userModel') 
 const Cryptr = require('cryptr')
 const cryptr = new Cryptr(process.env.CRYPTR_KEY? process.env.CRYPTR_KEY : require('../secrets/cryptr_key_secret'))
+const Cookie = require('js-cookie')
 
 const sendResponse = (res, err, data) => {
     if (err || !data || data.length == 0) 
         res.redirect('/error')
-    else 
-        res.redirect('/login')
+    else {
+        Cookie.set('HomeNotLogged_message', 'Email Verified!', { expires: 0.05 })
+        res.redirect('/home')
+    }
 }  
 
 const verifyUserController = {
@@ -19,7 +22,7 @@ const verifyUserController = {
             users.findOneAndUpdate(
                 { _id : userId }, 
                 { Verified : true},
-                (err, data) => sendResponse(res, err, "user verified!")
+                (err, data) => sendResponse(res, err, data)
             )
         }
         catch (err){
