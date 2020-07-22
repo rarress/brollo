894d32cd72ada461f10dfe6760438a5bfc1e3eec
u.rares <rarressro@gmail.com>
2020-07-22 16:41:34 +0300

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 import './App.css'
 
 import Topnav from './Topnav'
@@ -8,17 +9,30 @@ import Login from './Login'
 import Register from './Register'
 import ErrorPage from './ErrorPage'
 
-const App = () => { 
+const App = () => {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    axios.post('/api/checkToken').then(function (response) {
+      let userData = response.data
+      if (!userData.error)
+        setUser(userData)
+    }).catch(function (error) {
+      console.log('Error checking cookies');
+    })
+  }, []);
+
+
   return (
-  <>  
-    <Topnav/>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/login' component={Login}/>
-      <Route path='/register' component={Register}/>
-      <Route path='/error' component={ErrorPage}/>
-    </Switch> 
-  </>
+    <>
+      <Topnav />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/error' component={ErrorPage} />
+      </Switch>
+    </>
   )
 }
 
