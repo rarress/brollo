c1f2ae7329e643a9565df6979b9a6d9b55c66050
Cosmin <cosmin.aftanase@yahoo.com>
2020-07-24 15:39:33 +0300

    add /api/addFriend and /api/acceptFriend

--- server/models/friendsManagerModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const friendsManagerSchema = new mongoose.Schema({ 
-    Sender: String,
-    Receiver: String,
-    Status: Number // -1=block, 0=pending, 1=accepted
-});
-
-module.exports = mongoose.model('friendsManager', friendsManagerSchema, 'friendsManager')
