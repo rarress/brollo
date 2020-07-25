9b9f92aebef093fc3a72e4ab11772c768675646f
u.rares <rarressro@gmail.com>
2020-07-25 12:43:59 +0300

    added home when logged in

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,14 +4,14 @@ import axios from 'axios'
 import './App.css'
 
 import Topnav from './Topnav'
-import Home from './HomeNotLogged'
+import HomeNotLogged from './HomeNotLogged'
+import Home from './HomeWhenLogged'
 import Login from './Login' 
 import Register from './Register'
 import ErrorPage from './ErrorPage'
 
 const App = () => {
   const [user, setUser] = useState({})
-
   useEffect(() => {
     axios.post('/api/checkToken').then(function (response) {
       let userData = response.data
@@ -21,13 +21,12 @@ const App = () => {
       console.log('Error checking cookies');
     })
   }, []);
-
-
+  
   return (
     <>
       <Topnav />
       <Switch>
-        <Route exact path='/' component={Home} />
+        {Object.keys(user).length !== 0 ? <Route exact path='/' component={Home} /> : <Route exact path='/' component={HomeNotLogged} /> } 
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
         <Route path='/error' component={ErrorPage} />
