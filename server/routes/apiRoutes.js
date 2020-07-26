ec8cf2d76292fa43b9885d212278b6e29e6d8422
Cosmin <cosmin.aftanase@yahoo.com>
2020-07-26 21:44:23 +0300

    add board api

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -3,6 +3,7 @@ const loginController = require('../controllers/loginController')
 const verifyUserController = require('../controllers/verifyUserController')
 const checkToken = require('../controllers/checkToken')
 const friendsController = require('../controllers/friendsController')
+const boardsController = require('../controllers/boardsController')
 
 module.exports = app => {
     //COSMIN
@@ -10,11 +11,16 @@ module.exports = app => {
     app.post('/api/register', registerController.register)
     app.post('/api/addFriend/:user', friendsController.add)
     app.post('/api/acceptFriend/:user', friendsController.accept)
+    app.post('/api/boards', boardsController.create)
+    app.get('/api/boards/:id/user/:user', boardsController.getUserInfo)
+    app.get('/api/boards/:id', boardsController.read)
+    app.post('/api/boards/find', boardsController.find)
+    app.delete('/api/boards/:id', boardsController.delete)
 
-    //For testing to be removed
-    app.get('/api/random', async (req, res) => {
-        await new Promise(r => setTimeout(r, 5000));
-        res.send("random")
+    //For testing to be removed (heavy loading api)
+    app.get('/api/heavyLoading', async (req, res) => {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        res.send("some big data loaded!")
     })
 
     //RARES
