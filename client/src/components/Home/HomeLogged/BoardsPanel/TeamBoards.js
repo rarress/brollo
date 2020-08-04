3792cc278b223965f776ce1b303c5335477f2353
u.rares <rarressro@gmail.com>
2020-08-04 10:37:11 +0300

    Revert "Merge branch 'master' into rares"
    This reverts commit 7d47f2b48d75af83587dfc4147217b94d7af9ecc, reversing
    changes made to bf26a49e4c9ff4246f1451dac8332a15ea194485.

--- client/src/components/Home/HomeLogged/BoardsPanel/TeamBoards.js
+++ client/src/components/Home/HomeLogged/BoardsPanel/TeamBoards.js
@@ -2,10 +2,17 @@ import React from 'react'
 import Board from './Board'
 import { v4 as uuidv4 } from 'uuid'
 
-const TeamPanel = ({ name, boards}) => {  
+const TeamPanel = ({ name, boards}) => {
+    
+    const getNameGroup = () => {
+        if (name === "null")
+            return "Your boards:"
+        return `${name} boards:`
+    }
+
     return (
     <div className="teamBoards">
-        {name === "null"? "Your boards:" : `"${name}" boards:`}
+        {name === "null"? "Your boards:" : `\"${name}\" boards:`}
         <div className="teamBordsItems"> 
             { boards.map( (board) => 
                 <Board key={uuidv4()} prop={board}/>
