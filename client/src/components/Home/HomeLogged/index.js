620b2647e8cd6ea48c18ca298664ddab1d353406
u.rares <rarressro@gmail.com>
2020-08-08 14:45:24 +0300

    Revert "Merge branch 'master' into rares"
    This reverts commit 7d47f2b48d75af83587dfc4147217b94d7af9ecc, reversing
    changes made to bf26a49e4c9ff4246f1451dac8332a15ea194485.

--- client/src/components/Home/HomeLogged/index.js
+++ client/src/components/Home/HomeLogged/index.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react' 
 import BoardsPanel from './BoardsPanel/index.js'
+import { Col, Row, CardPanel} from 'react-materialize'
 import './homeLogged.css'
 
 const Home = ({user}) => { 
@@ -8,15 +9,14 @@ const Home = ({user}) => {
     document.body.style.backgroundImage = "" 
   }, [])
 
-  return (  
-    <div className="center"> 
-      <div className="homeContainer">  
-          <div className='panel'>
-            Teams 
-          </div>
-          <BoardsPanel user={user}/>
-      </div> 
-    </div>
+  return ( 
+  <Row>
+    <Col className="col s9 homeContainer">  
+        <CardPanel className='size2'>Teams</CardPanel> 
+        <BoardsPanel user={user}/>   
+        <CardPanel className='size2'>Friends</CardPanel> 
+    </Col>
+  </Row> 
   ); 
 }
 
